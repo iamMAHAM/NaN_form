3aa5698b65fbd33e728b8fc0496bea020078f290
MAHAM <abdulkabore@gmail.com>
2022-05-29 19:42:09 +0200

    update first version (splice and insert)

--- javaScript/extra-training/array.js
+++ javaScript/extra-training/array.js
@@ -11,22 +11,22 @@ class Arr
 		}
 	}
 
-	splice(index, nElement)
+	splice(index, nElement, item)
 	{
-		for (let i = index; i < index + nElement; i++)
-		{
-			console.log(this.delete(i))
+		if (index >= 0)
+		{		
+			if (nElement === 0){this.insert(index - 1, item)}
+			else{this.content[index - 1] = item}
 		}
-
-		// for (let i = 0; i < this.length; i++)
-		// {
-		// 	console.log(i)
-		// 	let properties = Object.getOwnPropertyNames(this.content)
-		// 	Object.defineProperty(this.content, properties[i] = i)
-		// }
+		else
+		{
+			index = this.length + index
+			if (nElement === 0){this.insert(index, item)}
+			else{this.content[index] = item}
+		}
 	}
 
-	selectMin()
+	min()
 	{
 		let min = this.content[0]
 		let index = 0
@@ -35,54 +35,23 @@ class Arr
 			if (min > this.content[i])
 			{
 				min = this.content[i]
-				index = i
 			}
 		}
-		return [min, index]
+		return min
 	}
 
-	selectMax()
+	max()
 	{
 		let max = this.content[0]
-		let index = 0
 		for (let i = 1; i < this.length; i++)
 		{
 			if (max < this.content[i])
 			{
 				max = this.content[i]
-				index = i
 			}
 		}
-		return [max, index]
-	}
-
-	sort(reverse=false)
-	{
-		let aux = []
-		if (reverse && typeof reverse === "boolean")
-		{
-			while (this.length > 0)
-			{
-				[number, index] = selectMax()
-				aux.push(number)
-				this.splice(index, 1)
-			}
-		}
-		else if (typeof reverse != "boolean")
-		{
-			return ("unexpected argument : did you mean true or false ?")
-		}
-		else
-		{
-			while (this.length > 0)
-			{
-				[number, index] = selectMin()
-				aux.push(number)
-				this.splice(index, 1)
-			}
-		}
-		return aux
-	}
+		return max
+	} 
 
 	join(separator)
 	{
@@ -96,28 +65,39 @@ class Arr
 
 	delete(index)
 	{
+		let deleted = this.content[index]
+		delete this.content[index]
 		for (let i = index; i < this.length - 1; i++)
 		{
+
 			this.content[i] = this.content[i + 1];
 		}
-		let deleted = this.content[this.length - 1];
-		delete this.content[this.length - 1];
+		delete this.content[this.length - 1]
 		this.length--;
 		return deleted;
 	}
 
 	push(item)
 	{
-		this.content[this.length] = item;
-		this.length++;
-		return this.length;
+		this.content[this.length] = item
+		this.length++
+		return this.length
 	}
-	
 
-
+	insert(index, item)
+	{
+		for (let i = this.length; i >= index; i--)
+		{
+		    this.content[i] = this.content[i - 1]
+		}
+		this.content[index] = item
+		this.length++
+		return this.content
+	}
 }
 
-let arr = new Arr("1er", "0", "lol", "je", 3, "lolefefe", "mangerbouger")
+let arr = new Arr("a", "b", "c", "d", "e", "f", 100, "m", "k", 'f')
 
-arr.splice(2, 3)
-console.log(arr.selectMin())
+arr.splice(-2, 1, "zoohien")
+console.log("array", arr)
+console.log(arr.max())
