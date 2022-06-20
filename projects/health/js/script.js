44edc87d6150f4a2a1bbf7b16916a16dc674edf1
MAHAM <abdulkabore@gmail.com>
2022-06-20 17:42:33 +0200

    add bookmark (add or remove )

--- projects/health/js/script.js
+++ projects/health/js/script.js
@@ -1,9 +1,13 @@
 /*Targetting elements*/
 
+let login = document.querySelector(".login")
+let bookmark = document.querySelector(".bookmark")
+let cart = document.querySelector(".cart")
 let search = document.querySelector("#search")
 let items = document.querySelector(".items")
-let addToCartButton = document.querySelectorAll(".add")
-
+let datas = null
+let remove = `<i class="fa-regular fa-trash-can"></i>`
+let add = `<i class="fa-regular fa-heart"></i>`
 /*declaring variables*/
 let res = null
 let baseUrl = "https://www.pharma-gdd.com/"
@@ -21,8 +25,26 @@ function redirectInfo(e)
 
 }
 
+function appropriateValues()
+{
+    let icone
+    let eventListener
+    if (window.location.href.includes("bookmark.html"))
+    {
+        icone = remove
+        eventListener = removeBookmark
+    }
+    else
+    {
+        icone = add
+        eventListener = addBookmark
+    }
+    return [icone, eventListener]
+}
+
 function fillPage(array)
 {
+    [icone, func] = appropriateValues()
     for (let i = 0; i < array.length; i++)
     {
         items.innerHTML += 
@@ -30,7 +52,7 @@ function fillPage(array)
         <div class="item" id="${i}" onclick="redirectInfo(event)">
             <div class="top">
                 <p class="status"><i class="fa-solid fa-circle-check"></i> en stock</p>
-                <p class="bookmark"><i class="fa-regular fa-heart"></i></p>
+                <p class="bookmark" onclick=func(event)>${icone}</p>
             </div>
             <img class="item-image" src="${array[i].img}" title="${array[i].redirect.split("-").join(" ").replace("/fr/", "")}">
             <div class="bottom">
@@ -45,10 +67,79 @@ function fillPage(array)
         `
     }
 }
+
+function fillBookmark(array)
+{
+    [icone, func] = appropriateValues()
+    for (let i = 0; i < array.length; i++)
+    {
+        items.innerHTML += array[i].replace(add, icone)
+    }
+}
+
+function removeBookmark(e)
+{
+    parent = e.target.parentElement.parentElement.parentElement.parentElement
+    child = e.target.parentElement.parentElement.parentElement
+    parent.removeChild(child)
+    toRemove = child.outerHTML.replace(remove, add)
+    console.log('\n\n')
+
+    for (let i = 0; i < datas.length; i++)
+    {
+        if (datas[i] === toRemove)
+        {
+            index = datas.indexOf(datas[i])
+            console.log("index found", index)
+            datas.splice(index, 1)
+        }
+    }
+    localStorage.setItem("bookmarks", JSON.stringify(datas))
+}
+
+function addBookmark(e)
+{
+    child = e.target.parentElement.parentElement.parentElement
+    datas = localStorage.getItem("bookmarks")
+    if (datas)
+    {
+        datas = JSON.parse(datas)
+        datas.push(child.outerHTML)
+    }
+    else
+    {
+        datas = []
+        datas.push(child.outerHTML)
+    }
+    localStorage.setItem("bookmarks", JSON.stringify(datas))
+}
 /*Event Listening*/
 window.addEventListener("load", async ()=>{
-    res = await fetch("back/datas.json");
-    res = await res.json()
-    fillPage(res)
-    console.log(res)
+
+
+    if (window.location.href.includes("index.html"))
+    {
+        res = await fetch("back/datas.json");
+        res = await res.json()
+        fillPage(res)
+    }
+    else {
+        if (localStorage.getItem("bookmarks"))
+        {
+            datas = localStorage.getItem("bookmarks")
+            datas = JSON.parse(datas)
+            fillBookmark(datas)
+        }
+    }
+    login.addEventListener("click", ()=>{
+        window.location.href = "../login-register.html"
+    })
+
+    bookmark.addEventListener("click", ()=>{
+        window.location.href = "../bookmark.html"
+    })
+
+    cart.addEventListener("click", ()=>{
+        window.location.href = "../cart.html"
+    })
 })
