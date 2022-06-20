8f51f1f98daae2e7c3502d54ce8679a1dc39bfc6
MAHAM <abdulkabore@gmail.com>
2022-06-20 23:00:52 +0200

    update details.js

--- projects/health/js/details.js
+++ projects/health/js/details.js
@@ -1,4 +1,6 @@
 
+let carts = null
+
 if(typeof(String.prototype.trim) === "undefined")
 {
     String.prototype.trim = function() 
@@ -10,7 +12,6 @@ if(typeof(String.prototype.trim) === "undefined")
 
 function detail()
 {
-    
     let container = document.querySelector(".container")
     let details = JSON.parse(localStorage.getItem("detail").trim())
     let content = 
@@ -44,4 +45,44 @@ function detail()
     container.innerHTML = content
 }
 
+function addToCart(e)
+{
+    console.log("start", e.target)
+    child = e.target.parentElement.parentElement.parentElement
+    carts = localStorage.getItem("carts")
+    if (carts)
+    {
+        carts = JSON.parse(carts)
+        carts.push(child.outerHTML.replace(add, remove).replace(inactive, remove))
+    }
+    else
+    {
+        carts = []
+        carts.push(child.outerHTML.replace(add, remove).replace(inactive, remove))
+    }
+    if (e.target.parentElement.classList.contains("active")){
+        e.target.parentElement.classList.remove("active")
+        e.target.parentElement.classList.add("inactive")
+        e.target.outerHTML = inactive
+    }
+    else{
+        e.target.parentElement.classList.remove("inactive")
+        e.target.parentElement.classList.add("active")
+        e.target.outerHTML = add
+    }
+    localStorage.setItem("carts", JSON.stringify(carts))
+}
+
+let toAdd = 
+`
+<div class="item" id="2">
+    <img class="item-image" src="${img}" title="${title-img}">
+    <div class="bottom">
+        <p class="title">${title}</p>
+        <p class="description">${description}</p>
+        <p class="prices">${price}<span class="m"> x ${quantities}</span></p>
+    </div>
+    <input oninput="updatePrices(event)" type="number" value="1" min="1" name="0" id="number">
+</div>
+`
 window.addEventListener("load", detail)
