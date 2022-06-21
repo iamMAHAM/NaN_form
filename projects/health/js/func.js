d10fba52a02d62815eb273743b56b1f563a69ba2
MAHAM <abdulkabore@gmail.com>
2022-06-21 12:59:29 +0000

    update func.js

--- projects/health/js/func.js
+++ projects/health/js/func.js
@@ -1,11 +1,19 @@
-window.addEventListener("load", () =>{
+function updateCartAmount()
+{
+    carts = JSON.parse(localStorage.getItem("carts"))
+    if (carts)
+        document.querySelector("div.total").textContent = carts.length
+}
 
+updateCartAmount()
+
+window.addEventListener("load", () =>{
     /**Targetting common things */
     let account = document.querySelector(".head.login")
     let bookmark = document.querySelector(".bookmark")
     let cart = document.querySelector(".cart")
     let home = document.querySelector(".top-header-center.head")
-    let search = document.querySelector(".search")
+    let search = document.querySelector("#search")
     /**EventListener */
     account.addEventListener("click", ()=>{
         window.location.href = "../login-register.html"
@@ -23,12 +31,3 @@ window.addEventListener("load", () =>{
         window.location.href = "../index.html"
     })
 })
-
-function updateCartAmount()
-{
-    carts = JSON.parse(localStorage.getItem("carts"))
-    if (carts)
-        document.querySelector("div.total").textContent = carts.length
-}
-
-updateCartAmount()
