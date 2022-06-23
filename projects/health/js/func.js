f81e0bbf534ef117c748fcdbd8d0537dd27bc864
MAHAM <abdulkabore@gmail.com>
2022-06-23 10:03:08 +0000

    move global function to func.js

--- projects/health/js/func.js
+++ projects/health/js/func.js
@@ -31,6 +31,44 @@ function logout()
     window.location.href = "../login-register.html"
 }
 
+function addToCart(e)
+{
+    parent = e.target.parentElement.parentElement.parentElement
+    console.log(parent)
+    imgLink = document.querySelector(".img").src
+    quantities = document.querySelector("#number").value
+    title = document.querySelector(".green").textContent
+    price = document.querySelector(".rt-price").textContent
+    description = document.querySelector(".center > p").textContent
+
+    let toAdd = 
+            `
+            <div class="item">
+                <img class="item-image" src="${imgLink}" title="${title}">
+                <div class="bottom">
+                    <p class="title">${title}</p>
+                    <p class="description">${description}</p>
+                    <input oninput="updatePrices(event)" type="number" value="${quantities}" min="1" name="0" id="number">
+                    <p class="prices">${price}<span class="m"> x ${quantities}</span></p>
+                </div>
+                <p class="icone-del" onclick="deleteFromCart(event)"><i class="fa-regular fa-trash-can"></i></p>
+            </div>
+            `
+    carts = localStorage.getItem("carts")
+    if (carts)
+    {
+        carts = JSON.parse(carts)
+        carts.push(toAdd)
+    }
+    else
+    {
+        carts = []
+        carts.push(toAdd)
+    }
+    localStorage.setItem("carts", JSON.stringify(carts))
+    updateCartAmount()
+}
+
 window.addEventListener("load", () =>{
     /**Targetting common things */
     let account = document.querySelector(".head.login")
