17455f93926a0220fe99c43878169288dd1b51c8
MAHAM <abdulkabore@gmail.com>
2022-06-25 08:06:22 +0000

    add slider

--- javaScript/week/slide/script.js
+++ javaScript/week/slide/script.js
@@ -1,16 +1,17 @@
+let slider = document.querySelector("div.slider")
 let container = document.querySelector(".container")
+let direction = null
 
-function fillContainer(imgs = [])
+function fillSlider(imgs = [])
 {
     for (let i  = 0; i < imgs.length; i++)
     {
-        container.innerHTML += 
+        ab =
+        `   <img class="img" src="${imgs[i]}" alt="">
         `
-            <img class="img" src="${imgs[i]}" alt="">
-        `
+        slider.innerHTML += ab
     }
 }
-
 
 window.addEventListener("load", async ()=>{
     let res = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=714e977060730eeb086fbb1570c15633&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
@@ -22,40 +23,37 @@ window.addEventListener("load", async ()=>{
     {
         images.push("https://image.tmdb.org/t/p/w500"+res.results[i].poster_path)
     }
-
-    fillContainer(images)
+    fillSlider(images)
 
     let prev = document.querySelector(".prev")
     let next = document.querySelector(".next")
-    let imgs = document.querySelectorAll(".img")
-    let length = imgs.length
-    let index = 0
-
-    imgs[index].classList.add("active")
 
     function prevImg(){
-        imgs[index].classList.remove("active")
-        if (index > 0){
-            index--;
-        }
-        else{
-            index = length - 1
-        }
-        imgs[index].classList.add("active")
+        if (direction === -1){container.style.direction = 1}
+        container.style.justifyContent = "flex-end"
+        slider.style.transform = `translate(100%)`
     }
 
     function nextImg(){
-        imgs[index].classList.remove("active")
-        if (index  < length - 1){
-            index++;
-        }
-        else{
-            index = 0
-        }
-        imgs[index].classList.add("active")
+        direction = -1
+        container.style.justifyContent = "flex-start"
+        slider.style.transform = `translate(-100%)`
     }
 
-    setInterval(nextImg, 2000)
+    // setInterval(nextImg, 2000)
     prev.addEventListener("click", prevImg)
     next.addEventListener("click", nextImg)
+
+    slider.addEventListener("transitionend", ()=>{
+        if (direction === 1){
+            slider.appendChild(slider.firstElementChild)
+        } else{
+            slider.prepend(slider.lastElementChild)
+        }
+        slider.style.transition = "none"
+        slider.style.transform = "translate(0)"
+        setTimeout(()=>{
+            slider.style.transition = "all 1s"
+        })
+    })
 })
