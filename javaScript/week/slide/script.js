486ee0866d36bf8af17ce74e3524a292dc3578c0
MAHAM <abdulkabore@gmail.com>
2022-06-29 18:05:20 +0200

    update script.js

--- javaScript/week/slide/script.js
+++ javaScript/week/slide/script.js
@@ -1,6 +1,7 @@
 let slider = document.querySelector("div.slider")
 let container = document.querySelector(".container")
 let direction = null
+let index = 0
 
 function fillSlider(imgs = [])
 {
@@ -29,37 +30,32 @@ window.addEventListener("load", async ()=>{
     let next = document.querySelector(".next")
 
 
-   const nextImg = ()=> {
-        direction = -1;
-        container.style.justifyContent = 'flex-start';
-        slider.style.transform = 'translate(-100%)';  
-      };
-      
-      const prevImg = ()=> {
-        if (direction === -1) {
-          direction = 1;
-          slider.appendChild(slider.firstElementChild);
-        }
-        container.style.justifyContent = 'flex-end';    
-        slider.style.transform = 'translate(100%)';  
-      };
+const nextImg = ()=> {
+	direction = -1
+	slider.style.transform = 'translate(-100%)';  
+	}
+	
+	const prevImg = ()=> {
+		direction = 1  
+		slider.style.transform = 'translate(100%)';  
+	}
 
-    prev.addEventListener("click", prevImg)
-    next.addEventListener("click", nextImg)
+	prev.addEventListener("click", prevImg)
+	next.addEventListener("click", nextImg)
 
-    slider.addEventListener('transitionend', ()=> {
-        
-        if (direction === 1) {
-          slider.prepend(slider.lastElementChild);
-        } else {
-          slider.appendChild(slider.firstElementChild);
-        }
-        
-        slider.style.transition = 'none';
-        slider.style.transform = 'translate(0)';
-        setTimeout(() => {
-          slider.style.transition = 'all 0.5s';
-        }, false)
+	slider.addEventListener('transitionend', ()=> {
+		
+	if (direction === 1) {
+	  slider.prepend(slider.lastElementChild);
+	} else {
+	  slider.appendChild(slider.firstElementChild);
+	}
+
+	slider.style.transition = 'none';
+	slider.style.transform = 'translate(0)';
+	setTimeout(() => {
+	slider.style.transition = 'all 0.5s';
+	}, false)
     })
 })
 
