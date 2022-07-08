6f28f44d6382ba55645b2b1c17f06616b2ae0a08
iamMAHAM <abdulkabore@gmail.com>
2022-07-08 13:37:40 +0000

    update files

--- javaScript/week/useless/routes.js
+++ javaScript/week/useless/routes.js
@@ -1,8 +1,21 @@
 const express = require("express")
+const axios = require("axios")
 const router = express.Router()
+const path = require("path")
+const fs = require("fs")
+let logos = []
+
+console.log(fs.readFile(path.join(__dirname, "links.json"), (err, data)=>{
+    if (err) throw err
+    logos = JSON.parse(data)
+    console.log(logos)
+}))
 
 router.get("/", (req, res)=>{
-    res.render("index")
+
+    res.render("index", {
+        logos : logos
+    })
 })
 
 router.use((req, res)=>{
