3b6766ee629cf023bea0f66b1ed41316a71452fc
iamMAHAM <abdulkabore@gmail.com>
2022-07-08 12:43:44 +0000

    update

--- javaScript/week/useless/scraper.py
+++ javaScript/week/useless/scraper.py
@@ -2,7 +2,7 @@ import json
 from bs4 import BeautifulSoup
 import requests
 
-res = requests.get("http://www.air-dg.com/")
+res = requests.get("")
 soup = BeautifulSoup(res.text, "html.parser")
 items = soup.select(".item img")
 print(items)
@@ -10,4 +10,4 @@ urls = []
 for item in items:
     urls.append(item["src"])
 
-open("links.json", "a").write(json.dumps(urls))
+open("links.json", "w").write(json.dumps(urls))
