95581e43dc35f17a4b2680a60e454e8dd4269c77
MAHAM <abdulkabore@gmail.com>
2022-07-17 08:08:20 +0000

    update server.js

--- javaScript/week/mysql/server.js
+++ javaScript/week/mysql/server.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const app = express()
 const routes = require("./routes/routes")
+const userRoutes = require("./routes/userRoutes")
 
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use("/", routes)
+app.use("/user", userRoutes)
 
-app.listen(3000, ()=>{
-    console.log("server started")
-})
+app.listen(3000, ()=>{console.log("server started")})
