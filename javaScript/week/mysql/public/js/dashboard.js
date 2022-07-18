c0c7ce3f15479a83060313a8f9364daf9d6b422c
MAHAM <abdulkabore@gmail.com>
2022-07-18 17:03:20 +0000

    update dashboard.js

--- javaScript/week/mysql/public/js/dashboard.js
+++ javaScript/week/mysql/public/js/dashboard.js
@@ -3,13 +3,15 @@ let infosdiv = document.querySelectorAll(".special")
 let del = document.querySelector("button.del")
 let form = document.querySelector("form")
 let cInfo = document.querySelector(".current-info")
+let update = document.querySelector(".update")
+let submit = document.querySelector(".submit")
 let users_info = null
 
 users_info = sessionStorage.getItem("session")
-console.log(users_info)
 if (users_info){
     try{
         users_info = JSON.parse(users_info)
+        cInfo.id = users_info.id
         cInfo.innerHTML = 
         `
             <p>Name : <span class="special" id="name">${users_info.name} </span></p>
@@ -25,18 +27,41 @@ if (users_info){
     window.location.href = "/"
 }
 
-window.addEventListener("DOMContentLoaded", ()=>{
-    del.addEventListener("click", async (e)=>{
-        e.preventDefault()
-        res = await fetch("/user/delete", {
-            method: "DELETE",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify({message: message})
-        })
-        res = await res.json()
-        window.location.href = "/"
+const fetchUpdate = async (e)=>{
+    let toUpdate = {}
+    e.preventDefault()
+    Array.from(update.children).forEach(child=>{
+        child = child.children[0]
+        toUpdate[child.name] = child.value
     })
-    // if (message.type === 'update'){location.reload()}
+    toUpdate["id"] = cInfo.id
+    res = await fetch("/user/update", {
+        method: 'POST',
+        body: JSON.stringify(toUpdate),
+        headers: {'content-type': 'application/json'}
+    })
+    res = await res.json()
+    sessionStorage.setItem("session", JSON.stringify(res.res[0]))
+    if (res){window.location.href = "/index"}
+}
+
+const deleteUser = async (e)=>{
+    e.preventDefault()
+    res = await fetch("/user/delete", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(users_info)
+    })
+    res = await res.json()
+    if (res.status === "ok"){
+        sessionStorage.removeItem("session")
+        window.location.href = "/"
+    }
+}
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    del.addEventListener("click", deleteUser)
+    submit.addEventListener("click", fetchUpdate)
 })
