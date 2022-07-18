5bf8b6485b4dbeea976b2081600faf24eee638de
MAHAM <abdulkabore@gmail.com>
2022-07-18 09:37:04 +0000

    update userController

--- javaScript/week/mysql/controllers/userController.js
+++ javaScript/week/mysql/controllers/userController.js
@@ -11,24 +11,29 @@ class userController{
 
     static insertUser = (req, res)=>{
         if (validate(req.body)){
-            if (users.createUser(req.body)){
-                res.send(JSON.stringify(template("ok", {message: 'account created successfully'})))
-            } else{
-                res.send(JSON.stringify(template("false", {message: 'error with database'})))
-            }
-        }else{
+            users.createUser(req.body, resl=>{
+                if (resl) res.send(JSON.stringify(template("ok", {message: 'account created successfully'})))
+                else res.send(JSON.stringify(template("false", {message: 'error with database'})))
+            })
+        }
+        else{
             res.send(template("false", {message: 'some incorrect entries'}))
         }
     }
 
     static loginUser = (req, res)=>{
-        console.log("new reqt login", req.body)
         users.checkLogin(req.body, (result)=>{
             if (result.res){
                 req.app.set("user", result)
-                res.redirect("/index")
+                res.send(JSON.stringify({
+                    status: "true",
+                    message: result.res
+                }))
             }
-            else res.render("login-register")
+            else res.send(JSON.stringify({
+                status: "false",
+                message: "bad credential"
+            }))
         })
     }
 
