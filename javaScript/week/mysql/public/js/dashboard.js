eae9ce14b3536612bf44b44093b899be8df098de
MAHAM <abdulkabore@gmail.com>
2022-07-18 09:37:28 +0000

    update dahsboard.js

--- javaScript/week/mysql/public/js/dashboard.js
+++ javaScript/week/mysql/public/js/dashboard.js
@@ -2,16 +2,30 @@ let infos = document.querySelector(".infos")
 let infosdiv = document.querySelectorAll(".special")
 let del = document.querySelector("button.del")
 let form = document.querySelector("form")
+let cInfo = document.querySelector(".current-info")
+let users_info = null
+
+users_info = sessionStorage.getItem("session")
+console.log(users_info)
+if (users_info){
+    try{
+        users_info = JSON.parse(users_info)
+        cInfo.innerHTML = 
+        `
+            <p>Name : <span class="special" id="name">${users_info.name} </span></p>
+            <p>surname : <span class="special" id="surname">${users_info.surname}</span></p>
+            <p>country : <span class="special" id="country">${users_info.country}</span></p>
+            <p>email : <span class="special" id="email">${users_info.email}</span></p>
+            <p>passwd : <span class="special" id="password">${users_info.password}</span></p>
+        `
+    } catch{
+        window.location.href = "/"
+    }
+}else{
+    window.location.href = "/"
+}
 
 window.addEventListener("DOMContentLoaded", ()=>{
-    let message = JSON.parse(infos?.textContent)
-    console.log(message.res)
-    console.log(infosdiv)
-    if (message instanceof Array){message = message.res[0]}
-    for (let i = 0; i < infosdiv.length; i++){
-        infosdiv[i].textContent = message.res[infosdiv[i].id]
-    }
-
     del.addEventListener("click", async (e)=>{
         e.preventDefault()
         res = await fetch("/user/delete", {
@@ -23,8 +37,6 @@ window.addEventListener("DOMContentLoaded", ()=>{
         })
         res = await res.json()
         window.location.href = "/"
-        
     })
-    console.log("type", message.type)
     // if (message.type === 'update'){location.reload()}
 })
