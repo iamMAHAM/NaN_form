e3041b122af1b820882a2376b18a6fbac5f3423d
iamMAHAM <abdulkabore@gmail.com>
2022-07-19 15:26:11 +0000

    update views

--- javaScript/week/student/controllers/userController.js
+++ javaScript/week/student/controllers/userController.js
@@ -1,4 +1,4 @@
-const users = require("../lib/usersLib")
+const usersLib = require("../lib/usersLib")
 const validate = require("../lib/validate")
 const template = (status="", data={})=>{
     return {
@@ -10,49 +10,54 @@ const template = (status="", data={})=>{
 class userController{
 
     static insertUser = (req, res)=>{
-        if (validate(req.body)){
-            usersLibcreateUser(req.body, resl=>{
-                if (resl) res.send(JSON.stringify(template("ok", {message: 'account created successfully'})))
-                else res.send(JSON.stringify(template("false", {message: 'error with database'})))
-            })
-        }
-        else{
-            res.send(template("false", {message: 'some incorrect entries'}))
-        }
+        console.log("insert", req.body)
+        validate(req.body)
+        // if (validate(req.body)){
+        //     usersLib.createUser(req.body, resl=>{
+        //         if (resl) res.send(JSON.stringify(template("ok", {message: 'account created successfully'})))
+        //         else res.send(JSON.stringify(template("false", {message: 'error with database'})))
+        //     })
+        // }
+        // else{
+        //     res.send(template("false", {message: 'some incorrect entries'}))
+        // }
     }
 
     static loginUser = (req, res)=>{
-        usersLibcheckLogin(req.body, (result)=>{
-            if (result.res){
-                req.app.set("user", result)
-                res.send(JSON.stringify({
-                    status: "true",
-                    message: result.res
-                }))
-            }
-            else res.send(JSON.stringify({
-                status: "false",
-                message: "bad credential"
-            }))
-        })
+        console.log("login", req.body)
+        // usersLib.checkLogin(req.body, (result)=>{
+        //     if (result.res){
+        //         req.app.set("user", result)
+        //         res.send(JSON.stringify({
+        //             status: "true",
+        //             message: result.res
+        //         }))
+        //     }
+        //     else res.send(JSON.stringify({
+        //         status: "false",
+        //         message: "bad credential"
+        //     }))
+        // })
     }
 
     static updateUser = (req, res)=>{
-        let user = null
-        for (let prop in req.body){
-            if (req.body[prop] !== '' && prop !== "id"){
-                usersLibUpdateInfo(prop, req.body[prop], req.body.id, (new_user)=>{
-                    console.log("new user", new_user)
-                    new_user !== undefined ? user = new_user : null
-                    res.send(JSON.stringify(new_user))
-                })
-            }
-        }
+        console.log("update", req.body)
+        // let user = null
+        // for (let prop in req.body){
+        //     if (req.body[prop] !== '' && prop !== "id"){
+        //         usersLib.UpdateInfo(prop, req.body[prop], req.body.id, (new_user)=>{
+        //             console.log("new user", new_user)
+        //             new_user !== undefined ? user = new_user : null
+        //             res.send(JSON.stringify(new_user))
+        //         })
+        //     }
+        // }
     }
 
     static deleteUser = (req, res)=>{
-        usersLibdeleteUser(req.body.id)
-        res.send(JSON.stringify({status: "ok"}))
+        console.log("delete", req.body)
+        // usersLib.deleteUser(req.body.id)
+        // res.send(JSON.stringify({status: "ok"}))
     }
 }
 
