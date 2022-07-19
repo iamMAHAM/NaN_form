d1e2f02d944bdb8300c30a6b2f80ba5300cf894a
iamMAHAM <abdulkabore@gmail.com>
2022-07-19 16:55:23 +0000

    update userController.js

--- javaScript/week/student/controllers/userController.js
+++ javaScript/week/student/controllers/userController.js
@@ -11,16 +11,15 @@ class userController{
 
     static insertUser = (req, res)=>{
         console.log("insert", req.body)
-        validate(req.body)
-        // if (validate(req.body)){
-        //     usersLib.createUser(req.body, resl=>{
-        //         if (resl) res.send(JSON.stringify(template("ok", {message: 'account created successfully'})))
-        //         else res.send(JSON.stringify(template("false", {message: 'error with database'})))
-        //     })
-        // }
-        // else{
-        //     res.send(template("false", {message: 'some incorrect entries'}))
-        // }
+        if (validate(req.body)){
+            usersLib.createUser(req.body, resl=>{
+                if (resl) res.send(JSON.stringify(template("ok", {message: 'account created successfully'})))
+                else res.send(JSON.stringify(template("false", {message: 'error with database'})))
+            })
+        }
+        else{
+            res.send(template("false", {message: 'some incorrect entries'}))
+        }
     }
 
     static loginUser = (req, res)=>{
