a28aee63670a3b36f82a78a259ca9761fd8d48eb
iamMAHAM <abdulkabore@gmail.com>
2022-07-19 15:27:00 +0000

    update dashboadr.js and route.js

--- javaScript/week/student/routes/routes.js
+++ javaScript/week/student/routes/routes.js
@@ -2,9 +2,8 @@ const router = require("express").Router()
 
 
 router.get("/", (req, res)=>{res.render("login")})
-
-router.get("/index", (req, res)=>{
-    res.render("dashboard")
+router.get("/register", (req, res)=>{res.render("register")})
+router.get("/index", (req, res)=>{res.render("dashboard")
 })
 
 module.exports = router
