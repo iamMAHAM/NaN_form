abfa373af2f0c304e9ea5ffead1a3017f5f977a5
iamMAHAM <abdulkabore@gmail.com>
2022-07-19 16:55:57 +0000

    update register.js

--- javaScript/week/student/public/js/register.js
+++ javaScript/week/student/public/js/register.js
@@ -1,6 +1,7 @@
 let coloneone = document?.querySelector(".column.one")
 let colonetwo = document?.querySelector(".column.two")
 let register = document?.querySelector(".register")
+let info = document?.querySelector(".error")
 
 const iterateParent = (parent, datas={})=>{
     Array.from(parent.children).forEach(child=>{
@@ -14,8 +15,15 @@ const registerF = async (e)=>{
     iterateParent(coloneone, datas)
     iterateParent(colonetwo, datas)
     res = await fetchData("/user/create", datas)
-    res = await res.json()
     console.log(res)
+    res.status === "false" ? info.style.color = "crimson" : info.style.color = "green"
+    info.textContent = res.results.message
+    info.style.display = "block"
+    setTimeout(()=>{
+        info.style.display = "none"
+    },3000)
+    res.status === "ok" ? window.location.href = "/login" : null
+
 }
 
 window.addEventListener("DOMContentLoaded", ()=>{
