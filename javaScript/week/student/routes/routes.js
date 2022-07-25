d69516cecf766ef2029991f65a5b729c5799a1f9
MAHAM <abdulkabore@gmail.com>
2022-07-25 16:00:36 +0000

    update routes.js

--- javaScript/week/student/routes/routes.js
+++ javaScript/week/student/routes/routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 
 
-router.get("/", (req, res)=>{res.render("login")})
+router.get("/", (req, res)=>{res.render("profile")})
 router.get("/register", (req, res)=>{res.render("register")})
 router.get("/index", (req, res)=>{res.render("dashboard")
 })
