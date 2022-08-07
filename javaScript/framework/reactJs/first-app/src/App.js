a78329ca754d2ea57120eb7c23fba127afe8be53
MAHAM <abdulkabore@gmail.com>
2022-08-07 19:22:23 +0000

    update App.js

--- javaScript/framework/reactJs/first-app/src/App.js
+++ javaScript/framework/reactJs/first-app/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import NavBar from './NavBar';
 import Home from './Home';
+import { BrowserRouter,Route, Routes} from "react-router-dom"
+import Add from './Add';
+import Detail from './Detail';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Home />
-    </div>
+    <BrowserRouter>
+        <div className="App">
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/add" element={<Add/>}></Route>
+            <Route path="/blogs/:id" element={<Detail/>}></Route>
+        </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
