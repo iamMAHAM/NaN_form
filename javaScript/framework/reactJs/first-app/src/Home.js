b2f63d3bef2c889f3a8889cfc75692d068ab8684
MAHAM <abdulkabore@gmail.com>
2022-08-08 15:12:40 +0000

    update home.js

--- javaScript/framework/reactJs/first-app/src/Home.js
+++ javaScript/framework/reactJs/first-app/src/Home.js
@@ -2,10 +2,10 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    const {data: blogsList, isloading, error} = useFetch("http://localhost:8000/users")
+    const {data: blogsList, isloading, error} = useFetch("http://localhost:8000/blogs")
     return (
         <div className="container">
-            {error && <div>Une erreur est survenue</div>} 
+            {error && <div>Une erreur est survenue</div>}
             {isloading && <div>Loading ...</div>}
             {blogsList && <BlogList blogs={ blogsList } title={"Liste total des blogs"}/>}
         </div> 
