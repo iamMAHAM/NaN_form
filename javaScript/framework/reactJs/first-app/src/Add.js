7ebd32bd23cbb48f914dd653e52789b799417bdb
MAHAM <abdulkabore@gmail.com>
2022-08-08 14:23:44 +0000

    update Add component

--- javaScript/framework/reactJs/first-app/src/Add.js
+++ javaScript/framework/reactJs/first-app/src/Add.js
@@ -2,6 +2,13 @@ const Add = () => {
     return ( 
         <div className="create">
             <h2>Add a new article</h2>
+            <div className="addFormular">
+                <form className="form">
+                    <input type="text" name="author" placeholder="author Name"/>
+                    <textarea name="content" cols="30" rows="10"></textarea>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
      );
 }
