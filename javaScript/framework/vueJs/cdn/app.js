4a5d2d9730e0c49356cbd84dec6063d8009c647c
iamMAHAM <abdulkabore@gmail.com>
2022-08-09 13:12:15 +0000

    using  vue.js with cdn

---
+++ javaScript/framework/vueJs/cdn/app.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            books: [
+                {
+                    title: "New Kid",
+                    author: "Jerry Craft",
+                    img: "assets/1.jpeg",
+                    isFav: true
+                },
+                {
+                    title: "Famous Book",
+                    author: "Jean Akoto",
+                    img: "assets/2.jpeg",
+                    isFav: false,
+                },
+                {
+                    title: "Wings of Fire",
+                    author: "Suntherdland",
+                    img: "assets/3.jpeg",
+                    isFav: true
+                },
+            ]
+        }
+    },
+    methods:{
+        toggleFav(book){
+            book.isFav = !book.isFav
+        }
+    },
+    computed: {
+        filterFavBooks(){
+            return this.books.filter(b => b.isFav)
+        }
+    }
+})
+
+app.mount("#app")
