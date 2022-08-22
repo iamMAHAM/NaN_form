3b328c0ae893b83b94e0e687dca9e01f3274c7f9
iamMAHAM <abdulkabore@gmail.com>
2022-08-22 11:22:52 +0000

    hot update

--- projects/health/src/lib/scraper.js
+++ projects/health/src/lib/scraper.js
@@ -1,5 +1,39 @@
 const axios = require("axios")
+const { JSDOM } = require('jsdom')
+const fs = require("fs")
+let cardInfos = []
 
-axios.get("http://google.com").then((res)=>{
-    console.log(res)
-})
+/*
+PHARMA DB
+title: div.querySelector(".product-brand") ? div.querySelector(".product-brand").textContent : '',
+image: div.querySelector(".thumbnail img").getAttribute("data-src"),
+description: div.querySelector(".product-subtitle").textContent,
+price: div.querySelector(".listing-price span").textContent
+
+*/
+const scraper = (
+    url="http://google.com",
+    selector,
+    state="",
+    title="",
+    image="",
+    description="",
+    price="",
+    )=>{
+        axios.get(url).then((res)=>{
+            const { document } = new JSDOM(res.data).window
+            document.querySelectorAll(selector).map(div=>{
+                    cardInfos.push({
+                        state: state ? state : '' ,
+                        title: title ? title : '',
+                        image: image,
+                        description: description,
+                        price: price
+                    })
+                })
+                fs.writeFile("data.json", JSON.stringify(cardInfos, null, space=4), (err)=>{
+                    err ? console.log(err) : console.log("write done !")
+                })
+            })
+}
+
