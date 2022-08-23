10284b391bd364280f29fd95d5f132ac2acadd24
iamMAHAM <abdulkabore@gmail.com>
2022-08-23 20:42:48 +0000

    update Cart.vue

--- projects/health/src/views/Cart.vue
+++ projects/health/src/views/Cart.vue
@@ -1,28 +1,85 @@
 <template>
-    <div class="cart">
-    <div class="cart-items">
-        <div class="cart-item">
-        <img class="item-image" src="/assets/med.png">
-        <div class="infos">
-            <span class="green">DOLIPRANE</span>
-            <small class="medoc-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
-            <input type="number" value="1">
-            <span class="article-price">0</span>
+    <div class="loading" v-if="loaded">Loading ...</div>
+    <div class="cart" v-if="!loaded">
+        <div class="cart-items" ref="cardItems">
+            <!-- <div class="cart-item" id="5.83">
+                 <i class="material-icons delete">delete</i>
+                <img class="item-image" src="../assets/med.png">
+                <div class="infos">
+                    <span class="green">DOLIPRANE</span>
+                    <small class="medoc-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
+                    <input
+                        type="number"
+                        min="1"
+                        max="10"
+                        :onchange="updateTotal"
+                    >
+                    <p class="article-price"> <span>{{ price }}</span> € * <span>{{ nb }}</span></p>
+                </div>
+            </div> -->
+        <div class="cart-item" v-for="cart in cartItems" :id="cart.id" :key="cart.id">
+            <i class="material-icons delete">delete</i>
+            <img class="item-image" :src=cart.image>
+            <div class="infos">
+                <span class="green">{{ cart.title }}</span>
+                <small class="medoc-desc">{{ cart.description }}</small>
+                <input
+                    type="number"
+                    min="1"
+                    max="10"
+                    :onchange="updateTotal"
+                >
+                <p class="article-price"> <span>{{ cart.price }}</span> € * <span>{{ cart.amount }}</span></p>
         </div>
         </div>
-    </div>
+        </div>
 
-    <div class="total-orders">
-        <span>TOTAL ORDERS</span>
-        <p>100000 €</p>
-        <button>order</button>
-    </div>
+        <div class="total-orders">
+            <span>TOTAL ORDERS</span>
+            <p>{{ total }} €</p>
+            <button>order</button>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue'
+import { unSaveDoc, getAll } from "@/lib/firestoreLib"
 
+export default {
+    name: 'Cart',
+    methods: {
+        update(){
+            const all = document.querySelectorAll(".article-price")
+            this.total = 0
+            Array.from(all).forEach(cmd=>{
+                this.total += eval(cmd.textContent.replace("€", ''))
+            })
+            this.total = this.total.toFixed(2)
+        },
+        updateTotal(e){
+            const target = e.target
+            console.log(target.nextElementSibling.children[1].textContent)
+            target.nextElementSibling.children[1].textContent = target.value
+            this.update()
+        }
+    },
+    data(){
+        return {
+            total: 0,
+            cartItems: [],
+            loaded: true
+        }
+    },
+    mounted(){
+        getAll("users/jhzGn5dE2kNLZOT4lUu2/cart", carts=> {
+            this.cartItems = carts
+            this.loaded = false
+            setTimeout(()=>{
+                this.update()
+            }, 2000)
+        })
+    }
 }
 </script>
 
@@ -41,6 +98,7 @@ export default {
     }
 
     div.cart-item{
+        position: relative;
         padding: 2rem;
         justify-content: space-around;
         border-bottom: .1rem solid var(--black);
@@ -86,11 +144,32 @@ export default {
     div.total-orders span{
         font-size: 2.2rem;
         padding: .3rem;
-        border-bottom: .1rem solid var(--white);
+        border-bottom: .2rem solid var(--white);
     }
 
     div.total-orders button{
-        margin: 0 auto;
+        cursor: pointer;
+        background-color: var(--white);
+        color: var(--black);
+        margin: .3rem auto;
+        font-size: 2.2rem;
+        border-radius: .5rem;
         width: 30%;
+    }
+
+    .delete{
+        cursor: pointer;
+        color: red;
+        position: absolute;
+        top: 1rem;
+        left: 1rem; 
+    }
+
+    .delete:hover{
+        background-color: var(--black);
+    }
+
+    .green{
+        color: var(--green)
     }
 </style>
