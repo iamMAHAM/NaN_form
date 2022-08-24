483c62956d9d1b34d2ed6643915bb48443ca9ec1
iamMAHAM <abdulkabore@gmail.com>
2022-08-24 13:05:00 +0000

    update sidebar.vue

--- projects/health/src/views/home/SideBar.vue
+++ projects/health/src/views/home/SideBar.vue
@@ -1,31 +1,32 @@
 <template>
 	<div class="side-bar">
+		<p class="cat">CATEGORIES</p>
 		<div class="side-content">
-			<router-link to="/drugs" class="side-row">
+			<router-link tag="div" to="/drugs" class="side-row">
 				<i class="material-icons">medication</i>
 				<span class="side-content">Drugs</span>
 			</router-link>
-			<router-link to="/healthy" class="side-row">
+			<router-link tag="div" to="/healthy" class="side-row">
 				<i class="material-icons">health_and_safety</i>
 				<span class="side-content">Health</span>
 			</router-link>
-			<router-link to="/beautyHi" class="side-row">
+			<router-link tag="div" to="/beautyHi" class="side-row">
 				<i class="material-icons">spa</i>
 				<span class="side-content">Beauty and Hygiene</span>
 			</router-link>
-			<router-link to="/babyP" class="side-row">
+			<router-link tag="div" to="/babyP" class="side-row">
 				<i class="material-icons">pregnant_woman</i>
 				<span class="side-content">Baby and Pregnancy</span>
 			</router-link>
-			<router-link to="/slimmingSport" class="side-row">
+			<router-link tag="div" to="/slimmingSport" class="side-row">
 				<i class="material-icons">sports_hockey</i>
 				<span class="side-content">Slimming and Sport</span>
 			</router-link>
-			<router-link to="/veterinary" class="side-row">
+			<router-link tag="div" to="/veterinary" class="side-row">
 				<i class="material-icons">pets</i>
 				<span class="side-content">Veterinary</span>
 			</router-link>
-			<router-link to="/medicalMaterials" class="side-row">
+			<router-link tag="div" to="/medicalMaterials" class="side-row">
 				<i class="material-icons">medical_services</i>
 				<span class="side-content">Medical Materials</span>
 			</router-link>
@@ -40,8 +41,15 @@ export default {
 </script>
 
 <style>
-
+	.cat{
+		color: var(--white);
+		background: var(--black);
+		padding: 1rem;
+		border-bottom: .1rem solid var(--white);
+		border-radius: .5rem .5rem 0 0;
+	}
 	div.side-bar{
+		text-align: center;
 		height: max-content;
 		position: sticky;
 		top: 30%;
@@ -56,10 +64,9 @@ export default {
 
 	div.side-content{
 		background-color: var(--black);
-		margin-top: 2rem;
 		padding: 1.5rem;
 		width: max-content;
-		border-radius: .5rem;
+		border-radius: 0 0 .5rem .5rem;
 	}
 
 	.side-row{
@@ -73,6 +80,10 @@ export default {
 
 	.side-row .material-icons{
 		margin: 0 .6rem;
+	}
+
+	.router-link-active{
+		color: var(--green);
 	}
 
 </style>
