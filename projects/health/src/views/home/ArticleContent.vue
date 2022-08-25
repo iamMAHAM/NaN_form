39373f1fa11a854905b50d3d495c817b5e3dda94
iamMAHAM <abdulkabore@gmail.com>
2022-08-25 13:18:28 +0000

    update article

--- projects/health/src/views/home/ArticleContent.vue
+++ projects/health/src/views/home/ArticleContent.vue
@@ -8,13 +8,15 @@
 			:key="card.id"
 			:card="card"
 			:path="$route.params.route"
+			@addFav="addFavs"
+			@removeFav="removeFavs"
 		/>
 	</div>
   </div>
 </template>
 
 <script>
-import { getAll } from '@/lib/firestoreLib';
+import { getAll, saveOrOverride, unSaveDoc } from '@/lib/firestoreLib';
 import Banner from './partials/Banner.vue';
 import Card from './partials/Card.vue';
 
@@ -24,16 +26,55 @@ export default {
 		Banner,
 		Card
 	},
+	updated(){
+		console.log("updated")
+	},
+	methods: {
+		addFavs: function (card){ // add to favorite
+			const index = this.cards.indexOf(card)
+			this.cards[index].load = true
+			const user =  JSON.parse(localStorage.getItem("user"))
+			if (!user){
+				this.$emit('fav')
+				return
+			}
+			saveOrOverride(`users/${user.id}/favorites`, card.id, card, ()=>{
+				this.cards[index].isFav = true
+				this.cards[index].load = false
+			})
+		},
+		removeFavs: async function (card){ // remove to favorite
+			const user =  JSON.parse(localStorage.getItem("user"))
+			const index = this.cards.indexOf(card)
+			this.cards[index].load = true
+			await unSaveDoc(`users/${user.id}/favorites`, card.id)
+			this.cards[index].isFav = false
+			this.cards[index].load = false
+
+		}
+	},
 	data(){
 		return {
 			cards: [],
-			isLoading: true
+			isLoading: true,
 		}
 	},
 	mounted(){
+		const user =  JSON.parse(localStorage.getItem("user"))
 		getAll(`data/Ho21xA8W3774097vSXhU/${this.$route.params.route}`, (result)=>{
-			this.cards = result
-			this.isLoading = false
+			if (user){
+				getAll(`users/${user.id}/favorites`, (favorites)=>{
+					result.map((c, i) => {
+						if (favorites.some(fav => c.id === fav.id )){
+							result[i].isFav = true
+						}else{
+							result[i].isFav = false
+						}
+					})
+					this.cards = result
+					this.isLoading = false
+				})
+			}
 		})
 	}
 }
