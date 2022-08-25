907b6a0e4eba68941328153b8363ba22396ced30
iamMAHAM <abdulkabore@gmail.com>
2022-08-25 18:23:22 +0000

    add Profile.vue

---
+++ projects/health/src/views/Profile.vue
@@ -0,0 +1,318 @@
+<template>
+  <div class="profile">
+        <table>
+            <tr>
+            <td>
+                <section>
+                    <label for="avatar">
+                    <i class="fa material-icons"
+						>
+						photo_camera
+					</i>
+                    <input
+						type="file"
+						id="avatar"
+						style="visibility:hidden;"
+						accept=".png,.jpg,jpeg,.PNG,.JPEG" 
+						name="avatar"
+					>
+                    </label>
+                    <img
+						src="https://raw.githubusercontent.com/iamMAHAM/NaN/master/javaScript/week/student/public/images/user_default.png"
+						id="preview"
+						alt="Avatar"
+					>
+                </section>
+                <h1>{{ form.firstName }} {{ form.lastName }}</h1>
+                <h3>{{ form.role }}</h3>
+            </td>
+            <td>
+                <ul id="fields" ref="ab">
+                    <li>
+                        <b>Full name</b>
+                        <input type="text" 
+							class="fields"
+							maxlength="100"
+							:value='`${form.firstName} ${form.lastName}`'
+						/>
+                        <i v-if="ok"
+							class="material-icons fa"
+						>
+							edit_square
+						</i>
+                    </li>
+                    <li ref="ab">
+                        <b>Email</b>
+                        <input type="email" 
+							class="fields"
+							maxlength="150"
+							v-model="form.email" 
+						/>
+                        <i class="material-icons fa"
+							>
+							edit_square
+						</i>
+                    </li>
+                    <li ref="ab">
+                        <b>Contact number</b>
+                        <input type="tel" 
+							class="fields"
+							maxlength="10"
+							v-model="form.contact"
+						/>
+                        <i class="material-icons fa"
+							>
+							edit_square
+						</i>
+                    </li>
+                    <li>
+                        <b>Birthday</b>
+                        <input
+							type="text" 
+							class="fields"
+							maxlength="250"
+							v-model="form.birth"
+						/>
+                        <i class="material-icons fa"
+							>
+							edit_square
+						</i>
+                    </li>
+
+                    <li>
+                        <b>Change Password</b>
+                        <input
+							type="password" 
+							class="fields"
+							maxlength="50"
+							v-model="form.password"
+						/>
+                        <i class="material-icons fa"
+							>
+							edit_square
+						</i>
+                    </li>
+                </ul>
+                <div
+					class="input"
+				>
+                    <input
+						type="submit"
+						value="submit"
+						id="submit"
+					>
+                </div>
+            </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'profile',
+	data(){
+		return {
+			form: {
+				email: 'dddd',
+				password: 'dddd',
+				birth: '01/01/2000',
+				contact: ''
+			},
+			ok: false,
+			edit_buttons: null,
+			edit_conf: null
+		}
+	},
+	mounted(){
+		this.edit_buttons = document.querySelectorAll(".fa")
+		const user = JSON.parse(localStorage.getItem("user"))
+		this.form = user
+		this.updateEvent(1, this.edit_buttons, this.editEvent)
+
+	},
+	methods:{
+		editEvent(e){
+			const target = e.target
+			const field = target.previousElementSibling
+			field.style.pointerEvents = "auto"
+			field.focus()
+			target.outerHTML = '<i class="fa material-icons">check</i>'
+			this.edit_conf = document.querySelectorAll(".fa")
+			this.updateEvent(0, this.edit_buttons, this.editEvent)
+    		this.updateEvent(1, this.edit_conf, this.closeEvent)
+		},
+		closeEvent(e){
+			const target = e.target
+			const field = target.previousElementSibling
+			field.style.pointerEvents = "none"
+			target.outerHTML = '<i class="fa material-icons">edit_square</i>'
+			this.edit_buttons = document.querySelectorAll(".fa")
+			this.updateEvent(0, this.edit_conf, this.closeEvent)
+			this.updateEvent(1, this.edit_buttons, this.editEvent)
+		},
+		updateEvent(flag, array, listener){
+			if (flag){
+				Array.from(array).forEach(child=>{
+					child.addEventListener("click", listener)
+				})
+			} else{
+				Array.from(array).forEach(child=>{
+					child.removeEventListener("click", listener)
+				})
+			}
+		}
+
+	}
+}
+</script>
+
+<style>
+
+.profile a, li, em, button, input, textarea, select{
+    text-decoration: none;
+    list-style: none;
+    font-style: normal;
+    outline: none !important;
+    transition: 0.5s;
+    resize: none;
+}
+
+ul{
+    position: relative;
+    left: -20px;
+}
+
+ul li{
+    padding: 20px 0;
+    color: gray; 
+    border-bottom:1px solid #1d1d1d;
+}
+
+b{
+    color:#fff;
+}
+
+.fa{
+    color:var(--green);
+    cursor: pointer;
+}
+
+.profile{
+    width:90%;
+    margin:10vh auto;
+}
+
+.profile table{
+    width:100%;
+}
+
+.profile table td{
+    margin:30px;
+    border-radius:5px;
+    box-shadow:0px 6px 16px -6px rgba(1,1,1,0.5);
+    padding:30px;
+    background-color:#000;
+    color:#fff;
+    vertical-align:top;
+}
+
+.profile table td:nth-child(1){
+    text-align:Center;
+}
+
+.profile table td:nth-child(2) .fa{
+    float:right;
+}
+
+.fields{
+    text-align: justify;
+    background:none;
+    font-size: 16px;
+    outline:none;
+    border:0;
+    color:gray;
+    width:60%;
+    pointer-events:none;
+}
+
+.profile table td:nth-child(1) section{
+    position:relative;
+    width:200px;
+    height:200px;
+    margin:5vh auto;
+}
+
+
+.profile table td:nth-child(1) .fa{
+    position:absolute;
+    right:25px;
+    top:25px;
+    font-size:2em;
+}
+
+#preview{
+    width:200px;
+    height:200px;
+    border-radius:50%;
+}
+
+.profile table td h3{
+    color:gray;
+    font-weight:normal;
+}
+
+.input{
+    text-align: center;
+}
+
+#submit{
+    color: black;
+    font-weight: bold;
+    cursor: pointer;
+    padding: 10px;
+    font-size: 18px;
+    width: auto;
+    background-color: var(--green);
+    border: .5px solid white;
+    border-radius: 5px;
+}
+
+@media (max-width:820px){
+    .profile{
+        width:100%;
+    }
+    .profile table td{
+        display:block;
+        width:90%;
+        margin:0px;
+    }
+    .profile table .section2 .quantityselector section{
+        width:41%;
+        margin:0px 10px;
+    }
+    .profile table .section2 ul{
+        position:relative;
+        left:-40px;
+    }
+
+}
+	.alert.success {background-color: #04AA6D;}
+	.alert.info {background-color: #2196F3;}
+	.alert.warning {background-color: #ff9800;}
+
+	.closebtn {
+		margin-left: 15px;
+		color: white;
+		font-weight: bold;
+		float: right;
+		font-size: 22px;
+		line-height: 20px;
+		cursor: pointer;
+		transition: 0.3s;
+	}
+
+	.closebtn:hover {
+		color: black;
+	}
+</style>
