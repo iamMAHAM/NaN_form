6d949a484326d51cc84e01bbb99978ede735ba6a
iamMAHAM <abdulkabore@gmail.com>
2022-10-07 14:35:36 +0000

    fix slide error

--- projects/immobilier/src/components/partials/Banner.vue
+++ projects/immobilier/src/components/partials/Banner.vue
@@ -16,18 +16,7 @@ export default {
     Categories,
     Carousel
   },
-  data(){
-    return {
-      slides:[
-        "assets/house.jpeg",
-        "https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?ca=6&ce=1&s=1024x768",
-        "assets/farm.jpeg",
-        "assets/hotel.webp",
-        "assets/fiable.jpeg",
-        "assets/accordconclu.webp",
-      ]
-    }
-  },
+  data(){ return { slides:[] } },
   methods:{
     emit(data){
       this.$emit('filter', data)
