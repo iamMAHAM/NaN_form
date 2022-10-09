4797e78de8b1155d9e426aa3f5ae28bd6a478cab
iamMAHAM <abdulkabore@gmail.com>
2022-10-09 10:04:09 +0000

    first learn

--- typescript/train/dist/app.js
+++ typescript/train/dist/app.js
@@ -1,12 +1,17 @@
 "use strict";
 const container = document.querySelector(".container");
-const button = document.querySelector("button");
-/**
- *
- * @param e
- */
-const increment = (e) => {
-    const target = e.target;
-    console.log(target.textContent);
-};
-button === null || button === void 0 ? void 0 : button.addEventListener('click', increment);
+const info = document.querySelector(".recoltedInfo");
+const submit = document.querySelector("input[type='submit']");
+const form = document.querySelector('form');
+submit === null || submit === void 0 ? void 0 : submit.addEventListener('click', (e) => {
+    const children = form === null || form === void 0 ? void 0 : form.elements;
+    e.preventDefault();
+    if (children) {
+        for (let i = 0; i < children.length; i++) {
+            const ab = children[i];
+            info
+                ? info.innerHTML += `<p>${ab.value}</p>`
+                : '';
+        }
+    }
+});
--- typescript/train/src/app.ts
+++ typescript/train/src/app.ts
@@ -1,13 +1,17 @@
-const container = document.querySelector(".container")
-const button = document.querySelector("button")
+const container = document.querySelector<HTMLDivElement>(".container")
+const info = document.querySelector<HTMLDivElement>(".recoltedInfo")
+const submit = document.querySelector<HTMLInputElement>("input[type='submit']")
+const form = document.querySelector('form')
 
-/**
- * 
- * @param e 
- */
-const increment = (e: Event)=>{
-    const target = e.target as Element
-    console.log(target.textContent)
-}
-
-button?.addEventListener('click', increment)
+submit?.addEventListener('click', (e: Event)=>{
+    const children = form?.elements
+    e.preventDefault()
+    if (children){
+        for (let i =0; i< children.length; i++){
+            const ab = children[i] as HTMLInputElement
+            info
+                ? info.innerHTML += `<p>${ab.value}</p>`
+                : ''
+        }
+    }
+})
